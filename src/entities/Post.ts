0a46f6bee9c25ec09650f8cba590f11639d33b46
TomiMoreno <tomimoreno03@gmail.com>
2021-05-21 20:31:42 -0300

    Initial posts crud

---
+++ src/entities/Post.ts
@@ -0,0 +1,23 @@
+import { Entity, Property, PrimaryKey } from "@mikro-orm/core";
+import {ObjectType, Field, Int} from 'type-graphql';
+@ObjectType()
+@Entity()
+export class Post {
+
+  @Field(() => Int)
+  @PrimaryKey()
+  id!: number;
+
+  @Field(() => String)
+  @Property({type: 'date'})
+  createdAt: Date = new Date();
+
+  @Field(() => String)
+  @Property({ onUpdate: () => new Date(), type:'date' })
+  updatedAt: Date = new Date();
+
+  @Field()
+  @Property({type: 'text'})
+  title!: string;
+
+}
